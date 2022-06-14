38c90e68879d3254ea4bb3c9cad7b677c5090756
JMS1208 <wjsalstjr59@gmail.com>
2022-06-14 21:16:39 +0900

    backup-master

--- app/src/main/java/com/jms/searchpharmacy/ui/view/home/DetailConvFragment.kt
+++ app/src/main/java/com/jms/searchpharmacy/ui/view/home/DetailConvFragment.kt
@@ -5,10 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.jms.searchpharmacy.R
+import com.jms.searchpharmacy.data.model.server.Convenience
+import com.jms.searchpharmacy.data.model.server.Pharmacy
 import com.jms.searchpharmacy.databinding.FragmentDetailConvBinding
+import com.jms.searchpharmacy.databinding.ItemDetailConvBinding
+import com.jms.searchpharmacy.databinding.ItemDetailPharBinding
 import com.jms.searchpharmacy.ui.view.MainActivity
 import com.jms.searchpharmacy.ui.viewmodel.MainViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class DetailConvFragment : Fragment() {
@@ -16,6 +25,36 @@ class DetailConvFragment : Fragment() {
     private val binding get() = _binding!!
     private val viewModel : MainViewModel by lazy {
         (activity as MainActivity).mainViewModel
+
+    }
+
+    private inner class DetailConvAdapter(val convList: List<Convenience>)
+        : RecyclerView.Adapter<DetailConvAdapter.DetailConvViewHolder>() {
+
+        inner class DetailConvViewHolder(val itemBinding: ItemDetailConvBinding)
+            : RecyclerView.ViewHolder(itemBinding.root) {
+
+            fun bind(conv: Convenience){
+                itemBinding.apply {
+
+                }
+            }
+
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailConvViewHolder {
+            val itemBinding = ItemDetailConvBinding.inflate(layoutInflater, parent, false)
+            return DetailConvViewHolder(itemBinding)
+        }
+
+        override fun onBindViewHolder(holder: DetailConvViewHolder, position: Int) {
+            val conv = convList[position]
+            holder.bind(conv)
+        }
+
+        override fun getItemCount(): Int = convList.size
+
+
     }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,7 +68,14 @@ class DetailConvFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.fetchedConvList.observe(viewLifecycleOwner) {
+            binding.convRv.adapter = DetailConvAdapter(it)
+            binding.convRv.layoutManager = LinearLayoutManager(requireContext())
 
+            for(i in 0 until if(it.size > 5) 5 else it.size) {
+                CoroutineScope(Dispatchers.IO).launch {
+                    viewModel.searchConvLoc(it[i].address)
+                }
+            }
         }
     }
 
