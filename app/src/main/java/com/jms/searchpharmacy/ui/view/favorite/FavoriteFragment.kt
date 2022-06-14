f9cfec36d64bbeee476827c7d45ce613fce903b5
JMS1208 <wjsalstjr59@gmail.com>
2022-06-14 09:26:27 +0900

    backup

--- app/src/main/java/com/jms/searchpharmacy/ui/view/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/jms/searchpharmacy/ui/view/favorite/FavoriteFragment.kt
@@ -5,13 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.jms.searchpharmacy.R
 import com.jms.searchpharmacy.databinding.FragmentFavoriteBinding
+import com.jms.searchpharmacy.databinding.ItemFavoriteBinding
+import com.jms.searchpharmacy.ui.view.MainActivity
+import com.jms.searchpharmacy.ui.viewmodel.MainViewModel
 
 class FavoriteFragment : Fragment() {
 
     private var _binding : FragmentFavoriteBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var favoriteViewModel: MainViewModel
+
+    private inner class FavoriteAdapter()
+        : RecyclerView.Adapter<FavoriteAdapter.FavoriteViewHolder>() {
+
+            inner class FavoriteViewHolder(binding: ItemFavoriteBinding)
+                : RecyclerView.ViewHolder(binding.root) {
+
+                    fun bind() {
+
+                    }
+            }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoriteViewHolder {
+            TODO("Not yet implemented")
+        }
+
+        override fun onBindViewHolder(holder: FavoriteViewHolder, position: Int) {
+            TODO("Not yet implemented")
+        }
+
+        override fun getItemCount(): Int {
+            TODO("Not yet implemented")
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,6 +52,12 @@ class FavoriteFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        favoriteViewModel = (activity as MainActivity).mainViewModel
+        //setupRecyclerView()
+
+    }
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
