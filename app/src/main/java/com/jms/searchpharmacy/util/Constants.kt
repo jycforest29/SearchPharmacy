7fd4ce8d85fba8f5abfe73b7d17c1077c20c06f2
jycforest29 <you@example.com>
2022-06-16 14:07:16 +0900

    ip 수정

--- app/src/main/java/com/jms/searchpharmacy/util/Constants.kt
+++ app/src/main/java/com/jms/searchpharmacy/util/Constants.kt
@@ -4,6 +4,6 @@ object Constants {
     const val CLIENT_ID = "yo55hrtdpw"
     const val CLIENT_SECRET = "chzlZ6A18o7c3XcHBCQ0JS69FZMTr8uQhUYpcB1S"
     const val NAVERMAP_BASE_URL="https://naveropenapi.apigw.ntruss.com/"
-    const val IP = "07a4-219-255-207-96.jp.ngrok.io"
+    const val IP = "ded8-220-72-249-253.jp.ngrok.io"
     const val SERVER_BASE_URL = "http://$IP/api/pharmacy/"
 }
